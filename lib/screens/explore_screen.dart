62920a9de08fa13714dbdf6a5b2d6df86e1eb27f
zigsong <wldms5764@gmail.com>
2024-07-08 11:43:50 +0900

    feat: explore screen에 Tab UI 구현

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -15,23 +15,61 @@ class _ExploreScreenState extends State<ExploreScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: FutureBuilder(
-            future: posts,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              }
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: Column(children: [
+          TabBar(
+            tabs: const [
+              Tab(text: "밴드찾기"),
+              Tab(text: "멤버찾기"),
+            ],
+            labelColor: Colors.amber[700],
+            dividerColor: Colors.amber[700],
+            indicatorColor: Colors.amber[700],
+            unselectedLabelColor: Colors.grey,
+          ),
+          const Expanded(
+            child: TabBarView(children: [
+              // ExploreBandContent(),
+              Center(child: Text("밴드를 찾아보세요")),
+              Center(child: Text("멤버를 찾아보세요")),
+            ]),
+          )
+        ]),
+      ),
+    );
+  }
+}
 
-              final posts = snapshot.data!;
-              return ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: ((context, index) {
-                    final post = posts[index];
-                    return ListTile(
-                      title: Text(post['title']),
-                    );
-                  }));
-            }));
+class ExploreBandContent extends StatefulWidget {
+  const ExploreBandContent({super.key});
+
+  @override
+  State<ExploreBandContent> createState() => _ExploreBandContentState();
+}
+
+class _ExploreBandContentState extends State<ExploreBandContent> {
+  final posts = Supabase.instance.client.from('posts').select('*');
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: posts,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final posts = snapshot.data!;
+          return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: ((context, index) {
+                final post = posts[index];
+                return ListTile(
+                  title: Text(post['title']),
+                );
+              }));
+        });
   }
 }
