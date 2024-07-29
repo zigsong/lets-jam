a925571d41485cd0940f5bf94e5f7233290b2cf5
zigsong <wldms5764@gmail.com>
2024-07-29 10:27:15 +0900

    feat: session 정보 tagCheckbox 마크업 및 enum list로 전송

---
+++ lib/models/session_enum.dart
@@ -0,0 +1,17 @@
+enum SessionEnum {
+  vocal,
+  drum,
+  guitar,
+  bass,
+  keyboard,
+  etc,
+}
+
+Map<SessionEnum, String> sessionMap = {
+  SessionEnum.vocal: '보컬',
+  SessionEnum.drum: '드럼',
+  SessionEnum.guitar: '기타',
+  SessionEnum.bass: '베이스',
+  SessionEnum.keyboard: '키보드',
+  SessionEnum.etc: '그 외',
+};
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lets_jam/models/session_enum.dart';
+import 'package:lets_jam/widgets/tag_checkbox.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -12,13 +14,17 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   final _formKey = GlobalKey<FormState>();
-  String sessionInfo = '';
+  List<SessionEnum> sessionInfo = [];
+  String nickname = '';
 
   void _submit() {
     if (_formKey.currentState?.validate() ?? false) {
       _formKey.currentState?.save();
       // Supabase에 정보 저장 로직
       _saveUserToSupabase();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('회원가입이 완료되었습니다')),
+      );
     }
   }
 
@@ -26,7 +32,8 @@ class _SignupScreenState extends State<SignupScreen> {
     try {
       final response = await Supabase.instance.client.from('users').insert({
         'email': widget.user.email,
-        'session': sessionInfo,
+        'nickname': nickname,
+        'sessions': sessionInfo.map((el) => el.name).toList(),
       });
     } catch (err) {
       print('회원가입 에러: $err');
@@ -48,16 +55,39 @@ class _SignupScreenState extends State<SignupScreen> {
           child: Column(
             children: [
               TextFormField(
-                decoration: const InputDecoration(labelText: '세션 입력'),
+                decoration: const InputDecoration(labelText: '닉네임'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return '세션을 입력하세요';
+                    return '닉네임을 입력하세요';
                   }
                   return null;
                 },
                 onSaved: (value) {
-                  sessionInfo = value ?? '';
+                  nickname = value ?? '';
                 },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '세션',
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  SessionCheckbox(
+                    onChange: (session) {
+                      if (sessionInfo.contains(session)) {
+                        sessionInfo.remove(session);
+                      } else {
+                        sessionInfo.add(session);
+                      }
+                    },
+                  ),
+                ],
               ),
               const SizedBox(height: 20),
               ElevatedButton(
@@ -69,5 +99,33 @@ class _SignupScreenState extends State<SignupScreen> {
         ),
       ),
     );
+  }
+}
+
+class SessionCheckbox extends StatefulWidget {
+  final Function(SessionEnum session) onChange;
+
+  const SessionCheckbox({super.key, required this.onChange});
+
+  @override
+  State<SessionCheckbox> createState() => _SessionCheckboxState();
+}
+
+class _SessionCheckboxState extends State<SessionCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+        children: SessionEnum.values
+            .map((session) => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: TagCheckbox(
+                    value: session,
+                    label: sessionMap[session] ?? '',
+                    onChange: (isChecked) {
+                      widget.onChange(session);
+                    },
+                  ),
+                ))
+            .toList());
   }
 }
