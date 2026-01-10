ac731cde974fbed6b96d2a19c44d50dc1efaa720
zigsong <wldms5764@gmail.com>
2026-01-10 17:39:04 +0900

    feat: 시간 표시 기준 변경

--- lib/utils/date_parser.dart
+++ lib/utils/date_parser.dart
@@ -1,17 +1,20 @@
 String getRelativeTime(DateTime timestamp) {
-  final now = DateTime.now(); // 현재 시간
-  final difference = now.difference(timestamp); // 현재 시간과의 차이 계산
+  final now = DateTime.now();
+  final difference = now.difference(timestamp);
 
   if (difference.inMinutes < 1) {
     return '방금 전';
   } else if (difference.inMinutes < 60) {
-    // 1시간 이내
     return '${difference.inMinutes}분 전';
   } else if (difference.inHours < 24) {
-    // 24시간 이내
     return '${difference.inHours}시간 전';
-  } else {
-    // 24시간 이상
+  } else if (difference.inDays < 7) {
     return '${difference.inDays}일 전';
+  } else if (difference.inDays < 30) {
+    return '${difference.inDays ~/ 7}주 전';
+  } else if (difference.inDays < 365) {
+    return '${difference.inDays ~/ 30}개월 전';
+  } else {
+    return '${difference.inDays ~/ 365}년 전';
   }
 }
