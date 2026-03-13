cc1ead78eba3ef1ba21de614fb5e86bdaa743464
zigsong <wldms5764@gmail.com>
2026-03-13 21:13:52 +0900

    feat: 알림 게시글 채우기

--- lib/screens/alarm_screen.dart
+++ lib/screens/alarm_screen.dart
@@ -15,16 +15,23 @@ class AlarmScreen extends StatelessWidget {
         iconTheme: IconThemeData(color: ColorSeed.boldOrangeMedium.color),
         title: Text(
           '알림',
-          style: TextStyle(
-              fontSize: 18, color: ColorSeed.boldOrangeMedium.color),
+          style:
+              TextStyle(fontSize: 18, color: ColorSeed.boldOrangeMedium.color),
         ),
       ),
       body: ListView.separated(
-        itemCount: 1,
+        itemCount: 2,
         itemBuilder: (context, index) {
+          if (index == 0) {
+            return ListTile(
+              title: const Text('JAM에 오신 걸 환영해요!'),
+              subtitle: const Text('JAM에서 함께 음악을 할 밴드와 멤버를 구해보세요 :)'),
+              onTap: () {},
+            );
+          }
           return ListTile(
-            title: const Text('JAM에 오신 걸 환영해요!'),
-            subtitle: const Text('JAM에서 함께 음악을 할 어쩌구'),
+            title: const Text('앱 업데이트 안내 2026ver.'),
+            subtitle: const Text('11.01 버전'),
             onTap: () {},
           );
         },
