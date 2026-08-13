5b2052d48ad5fcfaddb5a2414315fe66c4417f09
zigsong <wldms5764@gmail.com>
2026-08-14 08:03:22 +0900

    refactor: 프로필 작성 완료 시 버튼 로딩 처리

--- lib/widgets/wide_button.dart
+++ lib/widgets/wide_button.dart
@@ -5,18 +5,22 @@ class WideButton extends StatelessWidget {
   final String text;
   final VoidCallback? onPressed;
   final bool disabled;
+  final bool isLoading;
 
   const WideButton(
       {super.key,
       required this.text,
       required this.onPressed,
-      this.disabled = false});
+      this.disabled = false,
+      this.isLoading = false});
 
   @override
   Widget build(BuildContext context) {
+    final isDisabled = disabled || isLoading;
+
     return Container(
       width: MediaQuery.of(context).size.width,
-      decoration: disabled
+      decoration: isDisabled
           ? null
           : BoxDecoration(
               borderRadius: BorderRadius.circular(12),
@@ -29,22 +33,35 @@ class WideButton extends StatelessWidget {
               ],
             ),
       child: ElevatedButton(
-        onPressed: disabled ? null : onPressed,
+        onPressed: isDisabled ? null : onPressed,
         style: ElevatedButton.styleFrom(
           elevation: 0,
           backgroundColor: ColorSeed.boldOrangeStrong.color,
           foregroundColor: Colors.white,
-          disabledBackgroundColor: ColorSeed.meticulousGrayLight.color,
-          disabledForegroundColor: ColorSeed.meticulousGrayMedium.color,
+          disabledBackgroundColor: isLoading
+              ? ColorSeed.boldOrangeStrong.color
+              : ColorSeed.meticulousGrayLight.color,
+          disabledForegroundColor:
+              isLoading ? Colors.white : ColorSeed.meticulousGrayMedium.color,
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
         ),
-        child: Text(
-          text,
-          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-        ),
+        child: isLoading
+            ? const SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              )
+            : Text(
+                text,
+                style:
+                    const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+              ),
       ),
     );
   }
